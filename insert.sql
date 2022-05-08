31b8e22bcf21cee554dc4ad884eb0bab5c729a67
DaniloGuirra <daniloguirra@hotmail.com>
2022-05-08 03:31:19 -0300

    Inserir Insert

---
+++ insert.sql
@@ -0,0 +1,19 @@
+use SUCOS;
+
+insert into tbproduto(
+PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) 
+values ('1037797', 'Clean - 2 Litros - Laranja',
+'PET','2 Litros','Laranja', 16.01);
+
+insert into tbproduto(
+PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) 
+values ('1000889', 'Sabor da Montanha - 700 ml - Uva',
+'Garrafa','700 ml','Uva', 6.31);
+
+insert into tbproduto(
+PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) 
+values ('1004327', 'Videira do Campo - 1,5 Litros - Melancia',
+'PET','1,5 Litros','Melancia', 19.51);
+
+select * from tbproduto;
+
